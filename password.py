ae29e751f6e78a5b87a6c3e9b4e69e0bfa961ecd
mounika3223 <mounikalekkala525@gmail.com>
2024-08-28 15:22:49 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    length = int(input("Enter the desired length of the password: "))
+    password = generate_password(length)
+    print(f"Generated Password: {password}")
+
+main()
